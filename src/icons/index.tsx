7a28c45a6f1b411bc01bc5dfcf0d1afe60aab244
Najmu Syathir <134459622+najmusyathir@users.noreply.github.com>
2025-10-07 09:34:07 +0800

    Modify: Include icons in GH

---
+++ src/icons/index.tsx
@@ -0,0 +1,69 @@
+import GridIcon from "./grid.svg";
+import PlugInIcon from "./plug-in.svg";
+import ChevronDownIcon from "./chevron-down.svg";
+import HorizontaLDots from "./horizontal-dots.svg";
+import ChevronLeftIcon from "./chevron-left.svg";
+import EyeIcon from "./eye.svg";
+import EyeCloseIcon from "./eye-close.svg";
+import GroupIcon from "./group.svg";
+import TimeIcon from "./time.svg";
+import PencilIcon from "./pencil.svg";
+import PlusIcon from "./plus.svg";
+import FolderIcon from "./folder.svg";
+import CalenderIcon from "./calender-line.svg";
+import LockIcon from "./lock.svg";
+import UnlockIcon from "./user-line.svg";
+import UserIcon from "./user-circle.svg";
+import CalendarIcon from "./calendar.svg";
+import SearchIcon from "./list.svg";
+import UsersIcon from "./group.svg";
+import XIcon from "./close-line.svg";
+import CopyIcon from "./copy.svg";
+import EnvelopeIcon from "./envelope.svg";
+import MailLineIcon from "./mail-line.svg";
+import ShareIcon from "./paper-plane.svg";
+import SettingsIcon from "./pencil.svg";
+// import CloudUploadIcon from "./cloud-upload.svg";
+// import DocumentIcon from "./document.svg";
+import TrashIcon from "./trash.svg";
+// import DownloadIcon from "./download.svg";
+// import ArrowsUpDownIcon from "./arrows-up-down.svg";
+// import ArrowsUpIcon from "./arrows-up.svg";
+// import ArrowsDownIcon from "./arrows-down.svg";
+// import MagnifyingGlassIcon from "./magnifying-glass.svg";
+
+export {
+  GridIcon,
+  PlugInIcon,
+  ChevronDownIcon,
+  HorizontaLDots,
+  ChevronLeftIcon,
+  EyeIcon,
+  EyeCloseIcon,
+  GroupIcon,
+  TimeIcon,
+  PencilIcon,
+  PlusIcon,
+  FolderIcon,
+  CalenderIcon,
+  LockIcon,
+  UnlockIcon,
+  UserIcon,
+  CalendarIcon,
+  SearchIcon,
+  UsersIcon,
+  XIcon,
+  CopyIcon,
+  EnvelopeIcon,
+  MailLineIcon,
+  ShareIcon,
+  SettingsIcon,
+  // CloudUploadIcon,
+  // DocumentIcon,
+  TrashIcon,
+  // DownloadIcon,
+  // ArrowsUpDownIcon,
+  // ArrowsUpIcon,
+  // ArrowsDownIcon,
+  // MagnifyingGlassIcon,
+};
